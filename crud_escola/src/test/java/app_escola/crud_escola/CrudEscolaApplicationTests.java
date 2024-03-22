e70bd7c35f552da1ac04c4c558002484037cff16
HFVostok <hiago.fernandor7@gmail.com>
2024-03-21 21:31:41 -0300

    2024.03.21.00

---
+++ crud_escola/src/test/java/app_escola/crud_escola/CrudEscolaApplicationTests.java
@@ -0,0 +1,13 @@
+package app_escola.crud_escola;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudEscolaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
